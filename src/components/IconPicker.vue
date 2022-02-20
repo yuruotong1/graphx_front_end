7aef698a6bb867f4187a9ffa5085907533fbd659
ruotongyu <yuruotong@ceshiren.com>
2022-02-20 10:32:17 +0800

    实现图片选择器

---
+++ src/components/IconPicker.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-menu
+    v-model="menuVisible"
+    :close-on-content-click="false"
+    max-width="230px"
+    max-height="200px"
+    offset-x
+  >
+    <template v-slot:activator="{ on, attrs,}">
+        <slot name="menuActivator" :on="on" :attrs="attrs"> </slot>
+    </template>
+    <v-card>
+        <v-list>
+            <v-row dense class="flex">
+            <v-col
+                v-for="picture in this.pictures"
+                :key="picture.name"
+                cols="3"
+            >
+                <v-img 
+                  height="25px"
+                  contain
+                  :src="picture.avatar"
+                  class="img-padding" 
+                  :class="{'img-border': pickedPicture.avatar == picture.avatar}"
+                  @click=iconClick(picture)>
+              
+                </v-img>
+            </v-col>
+            </v-row>
+        </v-list>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="menuVisible=false"> Cancel </v-btn>
+            <v-btn color="primary" text @click="saveButton"> Save </v-btn>
+        </v-card-actions>
+        </v-card>
+  </v-menu>
+</template>
+
+<script >
+export default {
+  name: "IconPicker",
+  props: {
+      node: Object,
+  },
+  watch: {
+      // 清除 icon
+      menuVisible: function(x) {
+          if (!x) {
+              this.pictures=[];
+          }
+      }
+
+  },
+  data() {
+    return {
+      pictures: [],
+      menuVisible: false,
+      pickedPicture: null,
+    };
+  },
+  methods: {
+    addIcon(url, pictureName) {
+      this.pictures.push({avatar: url, pictureName: pictureName});
+    },
+
+    destoryIcon() {
+        this.pictures = [];
+    },
+
+    iconClick(picture) {
+        this.pickedPicture=picture;
+    },
+    saveButton() {
+        this.node.avatar=this.pickedPicture.avatar;
+
+        console.log(this.node);
+        this.menuVisible=false;
+    }
+  },
+};
+</script>
+<style scoped>
+  .img-border {
+      border: 3px solid red;
+  }
+  .img-padding {
+      padding: 3px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,150 +1,152 @@
 <template>
-
   <div>
     <v-row>
       <v-col cols="3">
-        <v-list> 
-          <v-list-item
-            v-for="node in graphData.nodeList"
-            :key="node.text"
-          >
+        <v-list>
+          <v-list-item v-for="node in graphData.nodeList" :key="node.text">
             <v-list-item-content>
               <v-list-item-title v-text="node.text"></v-list-item-title>
             </v-list-item-content>
             <v-list-item-avatar>
-              <v-menu offset-y :close-on-content-click=false :close-on-click=false v-model="menuInfo[node.id]">
-                <template v-slot:activator="{ on, attrs }">
-                   <v-img  
-                      :src="node.avatar" 
-                      v-bind="attrs"
-                      v-on="on"
-                      @click="imgClick(node)"
-                    />
+              <IconPicker :ref="'e-icon-picker-' + node.id" :node="node">
+                <template v-slot:menuActivator="{ on, attrs }">
+                  <v-img
+                    v-bind="attrs"
+                    v-on="on"
+                    :src="node.avatar"
+                    @click="imgClick(node)"
+                  />
                 </template>
-                <v-card>
-                  <v-card-title>选择图标</v-card-title>
-                  <v-divider></v-divider>
-                  <v-card-text style="height: 300px;">
-                    <e-icon-picker ref="iconPicker"  v-model="pickeredIconInfo[node.id]" :options="options"/>
-                  </v-card-text>
-                   <v-divider></v-divider>
-                    <v-card-actions>
-                      <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="menuInfo[node.id]=false"
-                      >
-                        Close
-                      </v-btn>
-                      <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="saveButton(node)"
-                      >
-                        Save
-                      </v-btn>
-                    </v-card-actions>
-                </v-card>
-             </v-menu>
+              </IconPicker>
             </v-list-item-avatar>
           </v-list-item>
         </v-list>
       </v-col>
       <v-col cols="7">
         <v-textarea
-        outlined
-        name="input-7-4"
-        label="输入表达式"
-        v-model="textareaValue"
-        height="300px"
-        style="margin:10px"
-      ></v-textarea>
-
+          outlined
+          name="input-7-4"
+          label="输入表达式"
+          v-model="textareaValue"
+          height="300px"
+          style="margin: 10px"
+        ></v-textarea>
       </v-col>
     </v-row>
-    <v-img :src="graphUrl"
-            width="800"
-            contain
-            style="margin-left: 400px"
-    ></v-img>
-  
+    <img
+      :src="graphUrl"
+      width="300px"
+      style="margin-left: 400px; margin-top: 0px"
+    />
   </div>
 </template>
 
 <script>
+import IconPicker from "../components/IconPicker";
 
-  export default {
-    name: 'Home',
-    data(){
-      return {
-        textareaValue: "",
-        graphData: {
-          nodeList: [],
-          edgeList: []},
-        graphUrl: "",
-        menuInfo: {},
-        options: {FontAwesome: false, ElementUI: false, addIconList: [], removeIconList: []},
-        pickeredIconInfo: {},
-    }
-    }, 
-    mounted() {
-
-    },
-    watch: {
-
-      textareaValue: function(val) {
-        this.refreshData();
-       }
-    },
-    methods: {
-      saveButton(node) {
-        node.avatar=this.pickeredIconInfo[node.id];
-        console.log("cur", node)
-        this.menuInfo[node.id]=false;
-        this.options.addIconList=[];
-        
-        this.refreshData();
-
+export default {
+  name: "Home",
+  data() {
+    return {
+      textareaValue: "",
+      graphData: {
+        nodeList: [],
+        edgeList: [],
       },
-      imgClick(node) {
-        this.axios({url:"https://iconsapi.com/api/search?appkey=620271bee4b06f79691875ea&query="+node.text, method: "GET"}).then((res) => {
-            res.data.pages.elements.forEach(element=>{
-              this.options.addIconList.push(element.url);
-            });
+      curPicture: "",
+      curNode: null,
+      graphUrl: "",
+      menuInfo: {},
+      options: { FontAwesome: false, ElementUI: false },
+      pickeredIconInfo: {},
+      captchaInputLastTime: null,
+      zIndex: 3500,
+      highLightColor: "#fc1944",
+    };
+  },
+  mounted() {},
+  watch: {
+    textareaValue: async function (val) {
+      var delay = 2000; //延迟2000 毫秒执行
+      this.captchaInputLastTime = new Date().valueOf();
+      await this.sleep(delay);
+      var nowTime = new Date().valueOf();
+      var gap = nowTime - this.captchaInputLastTime;
+      if (gap < delay) {
+        return;
+      }
+      this.refreshData();
+    },
+  },
+  methods: {
+    sleep(ms = 1000) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+    imgClick(node) {
+      this.curNode = node;
+      this.axios({
+        url:
+          "https://iconsapi.com/api/search?appkey=620271bee4b06f79691875ea&query=" +
+          node.text,
+        method: "GET",
+      }).then((res) => {
+        res.data.pages.elements.forEach((element) => {
+          this.$refs["e-icon-picker-" + node.id][0].addIcon(
+            element.url,
+            element.iconName
+          );
         });
-
-      },
-      refreshData() {
-        console.log(this.graphData)
-        this.axios({url:"/graph/parse", method: "POST", data: {"rawData": this.textareaValue, "jsonData": this.graphData}}).then((res) => {
-        if(res.status != 200 || res.data.success == false) {
+        if (this.$refs["e-icon-picker-" + node.id][0].pictures.length > 0) {
+          if (this.$refs["e-icon-picker-" + node.id][0].pickedPicture == null) {
+          this.$refs["e-icon-picker-" + node.id][0].pickedPicture =
+            this.$refs["e-icon-picker-" + node.id][0].pictures[0];
+          }
+        }
+      });
+    },
+    refreshData() {
+      this.axios({
+        url: "/graph/parse",
+        method: "POST",
+        data: { rawData: this.textareaValue, jsonData: this.graphData },
+      }).then((res) => {
+        if (
+          res.status != 200 ||
+          res.data.success == false ||
+          this.graphData == res.data.graphData
+        ) {
           return;
         }
-        this.graphData=res.data.graphData;
-        this.graphData.nodeList.forEach(node=>{
+        this.graphData = res.data.graphData;
+        this.graphData = res.data.graphData;
+        this.graphData.nodeList.forEach((node) => {
           this.$set(this.menuInfo, node.id, false);
-        })   
-        this.graphData.nodeList.forEach(node=>{
+        });
+        this.graphData.nodeList.forEach((node) => {
           this.$set(this.pickeredIconInfo, node.id, "");
-        })
-        this.axios({url:"/graph/getPng", method: "POST", data: this.graphData,  responseType: 'blob'}).then((res) => {
-                const blob = new Blob([res.data], {type: 'img/png'});
-                const url = window.URL.createObjectURL(blob);
-                this.graphUrl = url;
-                });
-       
-  })
-      }
+        });
+        this.axios({
+          url: "/graph/getPng",
+          method: "POST",
+          data: this.graphData,
+          responseType: "blob",
+        }).then((res) => {
+          const blob = new Blob([res.data], { type: "img/png" });
+          const url = window.URL.createObjectURL(blob);
+          this.graphUrl = url;
+        });
+      });
     },
-    components: {},
-  }
+  },
+  components: { IconPicker },
+};
 </script>
 
 <style scoped>
-  .my-custom-dialog {
-    position: absolute;
-    bottom: 50;
-    right: 50;
-    margin-left: 0px;
-  }
+.my-custom-dialog {
+  position: absolute;
+  bottom: 50;
+  right: 50;
+  margin-left: 0px;
+}
 </style>
